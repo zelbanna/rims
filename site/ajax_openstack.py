7467b7880d6f4314f508b8686a8662df0ae9d875
Juniper Stolabs <stolabs@juniper.net>
2017-06-09 13:59:28 +0200

    Tweaks

--- site/ajax_openstack.py
+++ site/ajax_openstack.py
@@ -232,15 +232,22 @@ def neutron_list(aWeb):
  print "<TR style='height:20px'><TD COLSPAN=3>"
  print "<A TITLE='Reload List' CLASS='z-btn z-small-btn z-op' OP=load DIV=div_navleft URL='ajax.cgi?call=openstack_neutron_list'><IMG SRC='images/btn-reboot.png'></A>"
  print "</TR>"
- print "<THEAD><TH>Owner</TH><TH>Network</TH><TH>Operations</TH></THEAD>"
+ print "<THEAD><TH>Network</TH><TH>Subnet</TH><TH>Operations</TH></THEAD>"
  for net in ret['data']['networks']:
-  if net['tenant_id'] == cookie.get('os_project_id'):
-   print "<TR><TD>{}</TD><TD>{}</TD>".format(pname,net['name'])
+  if net['tenant_id'] == cookie.get("os_project_id") or net['shared'] == True:
+   print "<TR>"
+   print "<TD>{}</TD>".format(net['name'])
+   print "<TD>"
+   for n in net['contrail:subnet_ipam']:
+    print n['subnet_cidr']
+   print "</TD>"
    print "<TD>"
    tmpl = "<A TITLE='{}' CLASS='z-btn z-op z-small-btn' DIV=div_navcont URL=ajax.cgi?call=openstack_neutron_action&name=" + net['name'] + "&id=" + net['id'] + "&op={} OP=load SPIN=true>{}</A>"
    print tmpl.format('Network info','info','<IMG SRC=images/btn-info.png>')  
-   print "</TD>"
-   print "</TR>"
+   print "</TD>" 
+  else:
+   print "<!-- Excluding {} -->".format(net['id'])
+ print "</TR>"
  print "</TABLE>"
  print "</DIV>"
 
@@ -329,6 +336,7 @@ def nova_action(aWeb):
   print "<DIV>"
   print tmpl.format('Details','details','Details')
   print tmpl.format('Diagnostics','diagnostics','Diagnostics')
+  print tmpl.format('Networks','networks','Networks')
   print "</DIV>"
 
   print "<DIV CLASS='z-table' style='overflow:auto' ID=div_os_info>"
@@ -346,6 +354,9 @@ def nova_action(aWeb):
   ret = controller.call(port,url + "/servers/{}/diagnostics".format(id))
   print "<H2>{}</H2>".format(name)
   _print_info(ret['data'])
+
+ elif op == 'networks':
+  print "networks"
 
  elif op == 'remove':
   ret = controller.call(port,url + "/servers/{}".format(id), method='DELETE')
@@ -374,6 +385,6 @@ def nova_console(aWeb):
  data = controller.call(cookie.get('os_nova_port'), cookie.get('os_nova_url') + "/servers/" + id + "/action", { "os-getVNCConsole": { "type": "novnc" } } )
  if data['code'] == 200:
   url = data['data']['console']['url']
-  # URL is #@?! inline URL.. remove http:// and replace IP (assume there is a port..) with controller IP
+  # URL is not always proxy ... so force it through: remove http:// and replace IP (assume there is a port..) with controller IP
   url = "http://" + cookie.get('os_controller') + ":" + url[7:].partition(':')[2]
   print "<iframe id='console_embed' src='{}&title={}' style='width: 100%; height: 100%;'></iframe>".format(url,name)
