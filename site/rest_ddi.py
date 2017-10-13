4ab34146abb73523993b03c70a8038c4c7433f99
Juniper Stolabs <stolabs@juniper.net>
2017-10-13 15:51:25 +0200

    Continuing DB transform

--- site/rest_ddi.py
+++ site/rest_ddi.py
@@ -8,8 +8,7 @@ __version__ = "17.10.4"
 __status__ = "Production"
 
 import sdcp.PackageContainer as PC
-import sdcp.core.GenLib as GL
-import sdcp.core.rest as REST
+import sdcp.core.genlib as GL
 
 ################################################ DDI - Common ################################################
 #
@@ -21,15 +20,14 @@ def load_infra(aDict):
  #
  domains = dns_domains(None) 
  subnets = ipam_subnets(None)
- db = GL.DB()
- db.connect()
- for dom in domains:
-  db.do("INSERT INTO domains(id,name) VALUES ({0},'{1}') ON DUPLICATE KEY UPDATE name='{1}'".format(dom['id'],dom['name']))
- db.commit()
- for sub in subnets:
-  db.do("INSERT INTO subnets(id,subnet,mask,subnet_description,section_id,section_name) VALUES ({0},{1},{2},'{3}',{4},'{5}') ON DUPLICATE KEY UPDATE subnet={1},mask={2}".format(sub['id'],sub['subnet'],sub['mask'],sub['description'],sub['section_id'],sub['section_name']))
- db.commit()
- db.close()
+ from sdcp.core.dbase import DB
+ with DB() as db:
+  for dom in domains:
+   db.do("INSERT INTO domains(id,name) VALUES ({0},'{1}') ON DUPLICATE KEY UPDATE name='{1}'".format(dom['id'],dom['name']))
+  db.commit()
+  for sub in subnets:
+   db.do("INSERT INTO subnets(id,subnet,mask,subnet_description,section_id,section_name) VALUES ({0},{1},{2},'{3}',{4},'{5}') ON DUPLICATE KEY UPDATE subnet={1},mask={2}".format(sub['id'],sub['subnet'],sub['mask'],sub['description'],sub['section_id'],sub['section_name']))
+  db.commit()
  return { 'domains':len(domains), 'subnets':len(subnets) }
 
 ################################################# DDI - DHCP #################################################
@@ -37,8 +35,10 @@ def load_infra(aDict):
 #
 #
 def dhcp_leases(aDict):
+ import sdcp.core.genlib as GL
  if PC.dhcp['proxy'] == 'True':
-  return REST.call(PC.dhcp['url'], "sdcp.site:ddi_dhcp_leases", aDict)
+  from sdcp.core.rest import call as rest_call
+  return rest_call(PC.dhcp['url'], "sdcp.site:ddi_dhcp_leases", aDict)
  active = []
  free   = []
  lease  = {}
@@ -72,7 +72,8 @@ def dhcp_update(aDict):
  # Update function - reload the DHCP server to use new info
  # - entries is a list of dict objects containing hostname, mac, ip etc
  if PC.dhcp['proxy'] == 'True':
-  return REST.call(PC.dhcp['url'], "sdcp.site:ddi_dhcp_update", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dhcp['url'], "sdcp.site:ddi_dhcp_update", aDict)
 
  entries = aDict['entries']
  # Create new file
@@ -100,22 +101,22 @@ def dhcp_update(aDict):
 #
 def dns_cleanup(aDict):
  if PC.dnsdb['proxy'] == 'True':
-  return REST.call(PC.dnsdb['url'], "sdcp.site:ddi_dns_cleanup", aDict) 
- db = GL.DB()
- db.connect_details('localhost',PC.dnsdb['username'],PC.dnsdb['password'],PC.dnsdb['dbname'])
- db.do("SELECT id,name,content FROM records WHERE type = 'A' OR type = 'PTR' ORDER BY name")   
- rows = db.get_rows();
- remove = []
- previous = {'content':None,'name':None}
- for row in rows:
-  if previous['content'] == row['content'] and previous['name'] == row['name']:
-   db.do("DELETE from records WHERE id = '{}'".format(row['id'] if row['id'] > previous['id'] else previous['id']))
-   row.pop('id')
-   remove.append(row)
-  else:              
-   previous = row
- db.commit()
- db.close()
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dnsdb['url'], "sdcp.site:ddi_dns_cleanup", aDict)
+ from sdcp.core.dbase import DB
+ with DB(PC.dnsdb['dbname'],'localhost',PC.dnsdb['username'],PC.dnsdb['password']) as db:
+  db.do("SELECT id,name,content FROM records WHERE type = 'A' OR type = 'PTR' ORDER BY name")   
+  rows = db.get_rows();
+  remove = []
+  previous = {'content':None,'name':None}
+  for row in rows:
+   if previous['content'] == row['content'] and previous['name'] == row['name']:
+    db.do("DELETE from records WHERE id = '{}'".format(row['id'] if row['id'] > previous['id'] else previous['id']))
+    row.pop('id')
+    remove.append(row)
+   else:              
+    previous = row
+  db.commit()
  return {'removed':remove}
 
 #
@@ -123,7 +124,9 @@ def dns_cleanup(aDict):
 #
 def dns_top(aDict):
  if PC.dnsdb['proxy'] == 'True':
-  return REST.call(PC.dnsdb['url'], "sdcp.site:ddi_dns_top", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dnsdb['url'], "sdcp.site:ddi_dns_top", aDict)
+ import sdcp.core.genlib as GL
  count = int(aDict.get('count',10))
  fqdn_top = {}                
  fqdn_who = {}
@@ -147,37 +150,39 @@ def dns_top(aDict):
 #
 def dns_domains(aDict):
  if PC.dnsdb['proxy'] == 'True':
-  return REST.call(PC.dnsdb['url'], "sdcp.site:ddi_dns_domains", aDict)
- db = GL.DB()
- db.connect_details('localhost',PC.dnsdb['username'], PC.dnsdb['password'], PC.dnsdb['dbname'])
- res = db.do("SELECT id, name FROM domains")
- db.close()
- return db.get_rows()
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dnsdb['url'], "sdcp.site:ddi_dns_domains", aDict)
+ from sdcp.core.dbase import DB
+ with DB(PC.dnsdb['dbname'],'localhost',PC.dnsdb['username'],PC.dnsdb['password']) as db:
+  res = db.do("SELECT id, name FROM domains")
+  rows = db.get_rows()
+ return rows
 
 #
 # lookup ( name, a_dom_id, ip)
 #
 def dns_lookup(aDict):
  if PC.dnsdb['proxy'] == 'True':
-  return REST.call(PC.dnsdb['url'], "sdcp.site:ddi_dns_lookup", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dnsdb['url'], "sdcp.site:ddi_dns_lookup", aDict)
+ import sdcp.core.genlib as GL
  ptr  = GL.ip2arpa(aDict['ip'])
  PC.log_msg("DNS  lookup - input:{}".format(aDict.values()))  
- db = GL.DB()
- db.connect_details('localhost',PC.dnsdb['username'], PC.dnsdb['password'], PC.dnsdb['dbname'])
- res = db.do("SELECT id,name FROM domains WHERE id = {} OR name = '{}'".format(aDict['a_dom_id'],ptr))
- domains = db.get_rows()
- if res == 2:
-  domain     = domains[0]['name'] if domains[1]['name'] == ptr else domains[1]['name']
-  ptr_dom_id = domains[1]['id']   if domains[1]['name'] == ptr else domains[0]['id']
- else:
-  domain     = domains[0]['name']
-  ptr_dom_id = '0'
- fqdn    = aDict['name'] + "." + domain
- db.do("SELECT id,content FROM records WHERE type = 'A' and domain_id = {} and name = '{}'".format(aDict['a_dom_id'],fqdn))
- a_record = db.get_row()
- db.do("SELECT id,content FROM records WHERE type = 'PTR' and domain_id = {} and name = '{}'".format(ptr_dom_id,GL.ip2ptr(aDict['ip']))) 
- p_record = db.get_row()
- db.close()
+ from sdcp.core.dbase import DB
+ with DB(PC.dnsdb['dbname'],'localhost',PC.dnsdb['username'],PC.dnsdb['password']) as db:
+  res = db.do("SELECT id,name FROM domains WHERE id = {} OR name = '{}'".format(aDict['a_dom_id'],ptr))
+  domains = db.get_rows()
+  if res == 2:
+   domain     = domains[0]['name'] if domains[1]['name'] == ptr else domains[1]['name']
+   ptr_dom_id = domains[1]['id']   if domains[1]['name'] == ptr else domains[0]['id']
+  else:
+   domain     = domains[0]['name']
+   ptr_dom_id = '0'
+  fqdn    = aDict['name'] + "." + domain
+  db.do("SELECT id,content FROM records WHERE type = 'A' and domain_id = {} and name = '{}'".format(aDict['a_dom_id'],fqdn))
+  a_record = db.get_row()
+  db.do("SELECT id,content FROM records WHERE type = 'PTR' and domain_id = {} and name = '{}'".format(ptr_dom_id,GL.ip2ptr(aDict['ip']))) 
+  p_record = db.get_row()
  retvals = {}
  if a_record and (a_record.get('content',None) == aDict['ip']):
   retvals['a_id'] = a_record.get('id') 
@@ -191,61 +196,62 @@ def dns_lookup(aDict):
 #
 def dns_update(aDict):
  if PC.dnsdb['proxy'] == 'True':
-  return REST.call(PC.dnsdb['url'], "sdcp.site:ddi_dns_update", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dnsdb['url'], "sdcp.site:ddi_dns_update", aDict)
  PC.log_msg("DNS  update - input:{}".format(aDict.values()))
+ import sdcp.core.genlib as GL
  from time import strftime
  serial  = strftime("%Y%m%d%H")
  ptr     = GL.ip2ptr(aDict['ip'])
  retvals = {}
- db = GL.DB()
- db.connect_details('localhost',PC.dnsdb['username'], PC.dnsdb['password'], PC.dnsdb['dbname'])
- res = db.do("SELECT id,name FROM domains WHERE id = {} OR name = '{}'".format(aDict['a_dom_id'],ptr.partition('.')[2]))
- domains = db.get_rows()
- if res == 2:
-  domain     = domains[0]['name'] if domains[1]['name'] == ptr else domains[1]['name']
-  ptr_dom_id = domains[1]['id']   if domains[1]['name'] == ptr else domains[0]['id']
- else:
-  domain     = domains[0]['name']
-  ptr_dom_id = None
- fqdn    = aDict['name'] + "." + domain
+ from sdcp.core.dbase import DB
+ with DB(PC.dnsdb['dbname'],'localhost',PC.dnsdb['username'],PC.dnsdb['password']) as db:
+  res = db.do("SELECT id,name FROM domains WHERE id = {} OR name = '{}'".format(aDict['a_dom_id'],ptr.partition('.')[2]))
+  domains = db.get_rows()
+  if res == 2:
+   domain     = domains[0]['name'] if domains[1]['name'] == ptr else domains[1]['name']
+   ptr_dom_id = domains[1]['id']   if domains[1]['name'] == ptr else domains[0]['id']
+  else:
+   domain     = domains[0]['name']
+   ptr_dom_id = None
+  fqdn    = aDict['name'] + "." + domain
 
- if aDict['a_id'] != '0':
-  retvals['a_op'] = "update"
-  retvals['a_id'] = aDict['a_id']
-  db.do("UPDATE records SET name = '{}', content = '{}', change_date='{}' WHERE id ='{}'".format(fqdn,aDict['ip'],serial,aDict['a_id']))
- else:
-  db.do("INSERT INTO records (domain_id,name,type,content,ttl,change_date) VALUES('{}','{}','A','{}',3600,'{}')".format(aDict['a_dom_id'],fqdn,aDict['ip'],serial))
-  retvals['a_op'] = "insert"
-  retvals['a_id'] = db.get_last_id()
+  if aDict['a_id'] != '0':
+   retvals['a_op'] = "update"
+   retvals['a_id'] = aDict['a_id']
+   db.do("UPDATE records SET name = '{}', content = '{}', change_date='{}' WHERE id ='{}'".format(fqdn,aDict['ip'],serial,aDict['a_id']))
+  else:
+   db.do("INSERT INTO records (domain_id,name,type,content,ttl,change_date) VALUES('{}','{}','A','{}',3600,'{}')".format(aDict['a_dom_id'],fqdn,aDict['ip'],serial))
+   retvals['a_op'] = "insert"
+   retvals['a_id'] = db.get_last_id()
 
- if aDict['ptr_id'] != '0':
-  retvals['ptr_id'] = aDict['ptr_id']
-  retvals['ptr_op'] = "update"
-  db.do("UPDATE records SET name = '{}', content = '{}', change_date='{}' WHERE id ='{}'".format(ptr,fqdn,serial,aDict['ptr_id']))
- elif aDict['ptr_id'] == '0' and ptr_dom_id:
-  db.do("INSERT INTO records (domain_id,name,type,content,ttl,change_date) VALUES('{}','{}','PTR','{}',3600,'{}')".format(ptr_dom_id,ptr,fqdn,serial))
-  retvals['ptr_op'] = "insert"
-  retvals['ptr_id'] = db.get_last_id()
+  if aDict['ptr_id'] != '0':
+   retvals['ptr_id'] = aDict['ptr_id']
+   retvals['ptr_op'] = "update"
+   db.do("UPDATE records SET name = '{}', content = '{}', change_date='{}' WHERE id ='{}'".format(ptr,fqdn,serial,aDict['ptr_id']))
+  elif aDict['ptr_id'] == '0' and ptr_dom_id:
+   db.do("INSERT INTO records (domain_id,name,type,content,ttl,change_date) VALUES('{}','{}','PTR','{}',3600,'{}')".format(ptr_dom_id,ptr,fqdn,serial))
+   retvals['ptr_op'] = "insert"
+   retvals['ptr_id'] = db.get_last_id()
 
- db.commit()
- db.close()
+  db.commit()
  PC.log_msg("DNS  update - results: " + str(retvals))
  return retvals
 
 def dns_remove(aDict):
  if PC.dnsdb['proxy'] == 'True':
-  return REST.call(PC.dnsdb['url'], "sdcp.site:ddi_dns_remove", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.dnsdb['url'], "sdcp.site:ddi_dns_remove", aDict)
  PC.log_msg("DNS  remove - input:{}".format(aDict.values()))
- db = GL.DB()
- db.connect_details('localhost',PC.dnsdb['username'], PC.dnsdb['password'], PC.dnsdb['dbname'])
  ares = 0
  pres = 0
- if aDict['a_id'] != '0':
-  ares = db.do("DELETE FROM records WHERE id = '{}' and type = 'A'".format(aDict['a_id']))
- if aDict['ptr_id'] != '0':
-  pres = db.do("DELETE FROM records WHERE id = '{}' and type = 'PTR'".format(aDict['ptr_id']))
- db.commit()
- db.close()
+ from sdcp.core.dbase import DB
+ with DB(PC.dnsdb['dbname'],'localhost',PC.dnsdb['username'],PC.dnsdb['password']) as db:
+  if aDict['a_id'] != '0':
+   ares = db.do("DELETE FROM records WHERE id = '{}' and type = 'A'".format(aDict['a_id']))
+  if aDict['ptr_id'] != '0':
+   pres = db.do("DELETE FROM records WHERE id = '{}' and type = 'PTR'".format(aDict['ptr_id']))
+  db.commit()
  PC.log_msg("DNS  remove - A:{} PTR:{}".format(str(ares),str(pres)))
  return { 'a':ares, 'ptr':pres }
 
@@ -255,27 +261,29 @@ def dns_remove(aDict):
 #
 def ipam_subnets(aDict):
  if PC.ipamdb['proxy'] == 'True':
-  return REST.call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_subnets", aDict)
- db = GL.DB()
- db.connect_details('localhost',PC.ipamdb['username'], PC.ipamdb['password'], PC.ipamdb['dbname'])
- res = db.do("SELECT subnets.id, subnet, mask, subnets.description, name as section_name, sectionId as section_id FROM subnets INNER JOIN sections on subnets.sectionId = sections.id") 
- db.close()
- return db.get_rows()
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_subnets", aDict)
+ from sdcp.core.dbase import DB
+ with DB(PC.ipamdb['dbname'],'localhost',PC.ipamdb['username'],PC.ipamdb['password']) as db:
+  db.do("SELECT subnets.id, subnet, mask, subnets.description, name as section_name, sectionId as section_id FROM subnets INNER JOIN sections on subnets.sectionId = sections.id") 
+  rows = db.get_rows()
+ return rows
 
 #
 # lookup(ip,ipam_sub_id)
 #
 def ipam_lookup(aDict):
  if PC.ipamdb['proxy'] == 'True':
-  return REST.call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_lookup", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_lookup", aDict)
  PC.log_msg("IPAM lookup - input {}".format(aDict.values()))
+ import sdcp.core.genlib as GL
  ipint   = GL.ip2int(aDict['ip'])
  retvals = { 'ipam_id':'0' }
- db = GL.DB()
- db.connect_details('localhost',PC.ipamdb['username'], PC.ipamdb['password'], PC.ipamdb['dbname'])
- db.do("SELECT id, dns_name, PTR FROM ipaddresses WHERE ip_addr = {0} AND subnetId = {1}".format(ipint,aDict.get('ipam_sub_id')))
- db.close()
- ipam   = db.get_row()
+ from sdcp.core.dbase import DB
+ with DB(PC.ipamdb['dbname'],'localhost',PC.ipamdb['username'],PC.ipamdb['password']) as db:
+  db.do("SELECT id, dns_name, PTR FROM ipaddresses WHERE ip_addr = {0} AND subnetId = {1}".format(ipint,aDict.get('ipam_sub_id')))
+  ipam   = db.get_row()
  if ipam:
   retvals['ipam_id'] = ipam.get('id')
   retvals['fqdn']    = ipam.get('dns_name',None)
@@ -287,30 +295,30 @@ def ipam_lookup(aDict):
 #
 def ipam_update(aDict):
  if PC.ipamdb['proxy'] == 'True':
-  return REST.call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_update", aDict)
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_update", aDict)
  PC.log_msg("IPAM update - input:{}".format(aDict.values()))
- db = GL.DB()
- db.connect_details('localhost',PC.ipamdb['username'], PC.ipamdb['password'], PC.ipamdb['dbname'])
  res = {}
-
- if aDict.get('ipam_id','0') != '0':
-  db.do("UPDATE ipaddresses SET PTR = '{}', dns_name = '{}' WHERE id = '{}'".format(aDict['ptr_id'],aDict['fqdn'],aDict['ipam_id']))
-  res['ipam_op'] = 'update'
-  res['ipam_id'] = aDict['ipam_id']
- else:
-  ipint = GL.ip2int(aDict['ip'])
-  db.do("SELECT id FROM ipaddresses WHERE ip_addr = '{0}' AND subnetId = {1}".format(ipint,aDict['ipam_sub_id']))
-  entry = db.get_row()
-  if entry:
-   db.do("UPDATE ipaddresses SET PTR = '{}', dns_name = '{}' WHERE id = '{}'".format(aDict['ptr_id'],aDict['fqdn'],entry['id']))
-   res['ipam_op'] = 'update_existed'
-   res['ipam_id'] = entry['id']
+ import sdcp.core.genlib as GL
+ from sdcp.core.dbase import DB
+ with DB(PC.ipamdb['dbname'],'localhost',PC.ipamdb['username'],PC.ipamdb['password']) as db:
+  if aDict.get('ipam_id','0') != '0':
+   db.do("UPDATE ipaddresses SET PTR = '{}', dns_name = '{}' WHERE id = '{}'".format(aDict['ptr_id'],aDict['fqdn'],aDict['ipam_id']))
+   res['ipam_op'] = 'update'
+   res['ipam_id'] = aDict['ipam_id']
   else:
-   db.do("INSERT INTO ipaddresses (subnetId,ip_addr,dns_name,PTR) VALUES('{}','{}','{}','{}')".format(aDict['ipam_sub_id'],ipint,aDict['fqdn'],aDict['ptr_id']))
-   res['ipam_op'] = "insert"
-   res['ipam_id'] = db.get_last_id()
- db.commit()
- db.close()
+   ipint = GL.ip2int(aDict['ip'])
+   db.do("SELECT id FROM ipaddresses WHERE ip_addr = '{0}' AND subnetId = {1}".format(ipint,aDict['ipam_sub_id']))
+   entry = db.get_row()
+   if entry:
+    db.do("UPDATE ipaddresses SET PTR = '{}', dns_name = '{}' WHERE id = '{}'".format(aDict['ptr_id'],aDict['fqdn'],entry['id']))
+    res['ipam_op'] = 'update_existed'
+    res['ipam_id'] = entry['id']
+   else:
+    db.do("INSERT INTO ipaddresses (subnetId,ip_addr,dns_name,PTR) VALUES('{}','{}','{}','{}')".format(aDict['ipam_sub_id'],ipint,aDict['fqdn'],aDict['ptr_id']))
+    res['ipam_op'] = "insert"
+    res['ipam_id'] = db.get_last_id()
+  db.commit()
  return res
 
 #
@@ -318,12 +326,12 @@ def ipam_update(aDict):
 #
 def ipam_remove(aDict):
  if PC.ipamdb['proxy'] == 'True':
-  return REST.call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_remove", aDict)
- db = GL.DB()
- db.connect_details('localhost',PC.ipamdb['username'], PC.ipamdb['password'], PC.ipamdb['dbname'])
- ires = db.do("DELETE FROM ipaddresses WHERE id = '{}'".format(aDict['ipam_id']))
- db.commit()
- db.close()
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_remove", aDict)
+ from sdcp.core.dbase import DB
+ with DB(PC.ipamdb['dbname'],'localhost',PC.ipamdb['username'],PC.ipamdb['password']) as db:
+  ires = db.do("DELETE FROM ipaddresses WHERE id = '{}'".format(aDict['ipam_id']))
+  db.commit()
  PC.log_msg("IPAM remove - {} -> {}".format(aDict,ires))
  return ires
 
@@ -332,14 +340,15 @@ def ipam_remove(aDict):
 #
 def ipam_find(aDict):
  if PC.ipamdb['proxy'] == 'True':
-  return REST.call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_find", aDict)
- db = GL.DB()
- db.connect_details('localhost',PC.ipamdb['username'], PC.ipamdb['password'], PC.ipamdb['dbname'])
- db.do("SELECT subnet, mask FROM subnets WHERE id = {}".format(aDict.get('ipam_sub_id'))) 
- sub = db.get_row()
- db.do("SELECT ip_addr FROM ipaddresses WHERE subnetId = {}".format(aDict.get('ipam_sub_id')))
- db.close()
- iplist = db.get_all_dict('ip_addr')
+  from sdcp.core.rest import call as rest_call             
+  return rest_call(PC.ipamdb['url'], "sdcp.site:ddi_ipam_find", aDict)
+ import sdcp.core.genlib as GL
+ from sdcp.core.dbase import DB
+ with DB(PC.ipamdb['dbname'],'localhost',PC.ipamdb['username'],PC.ipamdb['password']) as db:
+  db.do("SELECT subnet, mask FROM subnets WHERE id = {}".format(aDict.get('ipam_sub_id'))) 
+  sub = db.get_row()
+  db.do("SELECT ip_addr FROM ipaddresses WHERE subnetId = {}".format(aDict.get('ipam_sub_id')))
+  iplist = db.get_all_dict('ip_addr')
  subnet = int(sub.get('subnet'))
  start  = None
  ret    = { 'subnet':GL.int2ip(subnet) }
