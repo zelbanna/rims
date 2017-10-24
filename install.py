944e614be9090539260f99ebc601e13fb9cc9ac8
Juniper Stolabs <stolabs@juniper.net>
2017-10-24 08:25:20 +0200

    simplifications

--- install.py
+++ install.py
@@ -42,8 +42,8 @@ def install_sdcp(aFile):
  from shutil import copy
  imagedest = "{}/images/".format(PC.generic['docroot'])
  funcdest  = "{}/".format(PC.generic['docroot'])
- for file in listdir('site_images'):
-  copy("site_images/" + file, imagedest + file)
+ for file in listdir('images'):
+  copy("images/" + file, imagedest + file)
  for file in listdir('site_infra'):
   copy("site_infra/" + file, funcdest + file)
  print "\nCopied necessary files\n"
