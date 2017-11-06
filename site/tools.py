0744324867899949a87a4a2f6ed880fe28ba1d6d
Zacharias El Banna <zacharias@elbanna.se>
2017-11-06 10:21:44 +0100

    fix

--- site/tools.py
+++ site/tools.py
@@ -20,30 +20,29 @@ def main(aWeb):
  print "<A CLASS='z-op z-right' DIV=div_content URL='sdcp.cgi?call=resources_list_type&type=demo'>Demos</A>"
  print "<A CLASS='z-op z-right' DIV=div_content URL='sdcp.cgi?call=resources_list_type&type=tool'>Tools</A>"
  print "</DIV>"
- print "<DIV CLASS=z-content ID=div_content>"
- print "</DIV>"
+ print "<DIV CLASS=z-content ID=div_content></DIV>"
 
 def list(aWeb):
- print """<DIV CLASS=z-content-left ID=div_content_left>
-<DIV CLASS=z-frame><DIV CLASS=title>Tools</DIV>
-<DIV CLASS=z-table><DIV CLASS=tbody>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=dhcp_update'>Update DHCP Server</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=dns_load'>Load DNS Cache</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=dns_cleanup'>DNS Backend Cleanup</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=dns_discrepancy'>DNS  Backend Discrepancy</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=ipam_load'>Load IPAM Cache</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=ipam_discrepancy'>IPAM Backend Discrepancy</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op TARGET=_blank                  HREF='sdcp.cgi?call=device_dump_db'>Dump Device Table to JSON</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=rack_rackinfo'>View Rackinfo Table</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=device_mac_sync'>Find MAC Info</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=tools_mysql'>Dump DB Structure</A></DIV></DIV>
-<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=tools_sync_types'>Load New Types</A></DIV></DIV>"""
- print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right   URL='sdcp.cgi?call=tools_install&host=127.0.0.1'>Reinstall Host</A></DIV></DIV>"
+ print "<DIV CLASS=z-content-left ID=div_content_left>"
+ print "<DIV CLASS=z-frame><DIV CLASS=title>Tools</DIV>"
+ print "<DIV CLASS=z-table><DIV CLASS=tbody>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=dhcp_update'>Update DHCP Server</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=dns_load'>Load DNS Cache</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=dns_cleanup'>DNS Backend Cleanup</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=dns_discrepancy'>DNS  Backend Discrepancy</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=ipam_load'>Load IPAM Cache</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=ipam_discrepancy'>IPAM Backend Discrepancy</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op TARGET=_blank                  HREF='sdcp.cgi?call=device_dump_db'>Dump Device Table to JSON</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right SPIN=true URL='sdcp.cgi?call=rack_rackinfo'>View Rackinfo Table</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=device_mac_sync'>Find MAC Info</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=tools_mysql'>Dump DB Structure</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=tools_sync_types'>Load New Types</A></DIV></DIV>"
+ print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right           URL='sdcp.cgi?call=tools_install&host=127.0.0.1'>Reinstall Host</A></DIV></DIV>"
  from sdcp import PackageContainer as PC
  if PC.sdcp['svcsrv']:
   print "<DIV CLASS=tr><DIV CLASS=td><A CLASS=z-op DIV=div_content_right  URL='sdcp.cgi?call=tools_install&host={}'>Reinstall SVC</A></DIV></DIV>".format(PC.sdcp['svcsrv'])
-print "<DIV></DIV>"
-print "<DIV CLASS=z-content-right ID=div_content_right></DIV>"
+ print "</DIV></DIV></DIV></DIV>"
+ print "<DIV CLASS=z-content-right ID=div_content_right></DIV>"
 
 #
 #
@@ -68,7 +67,8 @@ def sync_types(aWeb):
 #
 def install(aWeb):
  from sdcp.core.rest import call as rest_call
- print "<DIV CLASS=z-frame>"
+ from json import dumps
+ print "<DIV CLASS=z-frame><PRE>"
  res = rest_call("http://{}/rest.cgi".format(aWeb['host']),"sdcp.rest.tools_installation")
- print res
- print "</DIV>"
+ print dumps(res,indent=4)
+ print "</PRE></DIV>"
