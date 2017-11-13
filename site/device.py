93ae0a36d657260ff61afa9f6814469ec6c5003e
Zacharias El Banna <zacharias@elbanna.se>
2017-11-13 22:04:21 +0100

    prep ipam_sub_id

--- site/device.py
+++ site/device.py
@@ -343,7 +343,7 @@ def new(aWeb):
  name   = aWeb.get('hostname','unknown')
  mac    = aWeb.get('mac',"00:00:00:00:00:00")
  op     = aWeb['op']
- sub_id = aWeb['ipam_sub_id']
+ ipam_sub_id = aWeb['ipam_sub_id']
  if not ip:
   from sdcp.core import genlib as GL
   ip = "127.0.0.1" if not aWeb['ipint'] else GL.int2ip(int(aWeb['ipint']))
@@ -354,7 +354,7 @@ def new(aWeb):
 
   a_dom_id,_,domain = aWeb['a_dom_id'].partition('_')
   fqdn = "{}.{}".format(name,domain)
-  args = { 'ip':ip, 'mac':mac, 'hostname':name, 'fqdn':fqdn, 'a_dom_id':a_dom_id, 'ipam_sub_id':sub_id }
+  args = { 'ip':ip, 'mac':mac, 'hostname':name, 'fqdn':fqdn, 'a_dom_id':a_dom_id, 'ipam_sub_id':ipam_sub_id }
 
   if aWeb['vm']:
    args['vm'] = 1
@@ -366,7 +366,7 @@ def new(aWeb):
   print "DB:{}".format(res)
   aWeb.log("{} - 'new device' operation:[{}] -> [{}]".format(aWeb.cookie.get('sdcp_user'),args,res))
  elif op == 'find':
-  res  = sdcpipam.find({'id':sub_id})
+  res  = sdcpipam.find({'id':ipam_sub_id})
   print "IP:{}".format(res['ip'])
  else:
   domain = aWeb['domain']
@@ -388,7 +388,7 @@ def new(aWeb):
   print "</SELECT></DIV></DIV>"
   print "<DIV CLASS=tr><DIV CLASS=td>Subnet:</DIV><DIV CLASS=td><SELECT NAME=ipam_sub_id>"
   for s in subnets:
-   print "<OPTION VALUE={} {}>{} ({})</OPTION>".format(s['id'],"selected" if str(s['id']) == sub_id else "", s['subnet'],s['description'])
+   print "<OPTION VALUE={} {}>{} ({})</OPTION>".format(s['id'],"selected" if str(s['id']) == ipam_sub_id else "", s['subnet'],s['description'])
   print "</SELECT></DIV></DIV>"
   print "<DIV CLASS=tr><DIV CLASS=td>MAC:</DIV><DIV CLASS=td><INPUT NAME=mac TYPE=TEXT PLACEHOLDER='{0}'></DIV></DIV>".format(mac)
   if aWeb['target'] == 'rack_id':
@@ -430,7 +430,7 @@ def discover(aWeb):
   from sdcp.rest.device import discover
   clear = aWeb.get('clear',False)
   a_dom = aWeb['a_dom_id']
-  ipam  = aWeb.get('ipam_sub',"0_0_32").split('_')
+  ipam  = aWeb.get('ipam_subnet',"0_0_32").split('_')
   # id, subnet int, subnet mask
   res = discover({ 'ipam_sub_id':ipam[0], 'ipam_mask':ipam[2], 'start':int(ipam[1]), 'end':int(ipam[1]) + 2**(32-int(ipam[2])) - 1, 'a_dom_id':a_dom, 'clear':clear})
   print "<DIV CLASS=z-frame>{}</DIV>".format(res)
@@ -452,7 +452,7 @@ def discover(aWeb):
     extra = "" if not dom_name == d.get('name') else "selected=selected"
     print "<OPTION VALUE={0} {2}>{1}</OPTION>".format(d.get('id'),d.get('name'),extra)
   print "</SELECT></DIV></DIV>"
-  print "<DIV CLASS=tr><DIV CLASS=td>Subnet:</DIV><DIV CLASS=td><SELECT NAME=ipam_sub>"
+  print "<DIV CLASS=tr><DIV CLASS=td>Subnet:</DIV><DIV CLASS=td><SELECT NAME=ipam_subnet>"
   for s in subnets:
    print "<OPTION VALUE={0}_{1}_{3}>{2}/{3} ({4})</OPTION>".format(s.get('id'),s.get('subnet'),s.get('subasc'),s.get('mask'),s.get('description'))
   print "</SELECT></DIV></DIV>"
