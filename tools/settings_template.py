e2c753e9563dadd5fff6e7ee8c8fb0a4881215c1
Zacharias El Banna <zacharias@elbanna.se>
2018-02-08 00:15:39 +0100

    fixes

--- tools/settings_template.py
+++ tools/settings_template.py
@@ -13,29 +13,25 @@ if __name__ == "__main__":
  if len(argv) < 2:
   print argv[0] + " <output file>"
   exit(0)
-
- from sdcp.core.dbase import DB
- config = {}
- with DB() as db:
-  db.do("SELECT DISTINCT section FROM settings")
-  sections = db.get_rows()
-  for section in sections:
-   sect = section['section']
-   config[sect] = {}
-   db.do("SELECT parameter,value,description,required FROM settings WHERE section = '%s' ORDER BY parameter"%sect)
-   params = db.get_rows()
-   for param in params:
-    key = param.pop('parameter',None)
-    if key in ['username', 'password', 'encrypted','url','domain']:
-     param['value'] = key
-    config[sect][key] = param
- try:
-  from json import load,dump, dumps
-  with open(argv[1],'w') as f:
-   dump(config,f,indent=4,sort_keys=True)
- except:
-  print "Error saving file"
-  exit(1)
  else:
-  print "Saved template file"
- exit(0)
+  from sdcp.core.dbase import DB
+  config = {}
+  with DB() as db:
+   db.do("SELECT DISTINCT section FROM settings")
+   sections = db.get_rows()
+   for section in sections:
+    sect = section['section']
+    config[sect] = {}
+    db.do("SELECT parameter,value,description,required FROM settings WHERE section = '%s' ORDER BY parameter"%sect)
+    params = db.get_rows()
+    for param in params:
+     key = param.pop('parameter',None)
+     if key in ['username', 'password', 'encrypted','url','domain']:
+      param['value'] = key.upper()
+     config[sect][key] = param
+  try:
+   from json import dump
+   with open(argv[1],'w') as f:
+    dump(config,f,indent=4,sort_keys=True)
+  except:
+   print "Error saving file"
