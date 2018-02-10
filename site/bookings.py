e6976231420e2052c1d6f5dbae721b3f3a40b57b
Zacharias El Banna <zacharias@elbanna.se>
2018-02-11 00:50:47 +0100

    fixes

--- site/bookings.py
+++ site/bookings.py
@@ -27,7 +27,7 @@ def list(aWeb):
  print "<DIV CLASS=thead><DIV CLASS=th>User (Id)</DIV><DIV CLASS=th>Device</DIV><DIV CLASS=th>Until</DIV><DIV CLASS=th>Op</DIV></DIV>"
  print "<DIV CLASS=tbody>"
  for row in rows:
-  print "<DIV CLASS=tr><DIV CLASS=td><A CLASS='z-op' DIV=div_content_right URL='sdcp.cgi?call=users_info&id={3}'>{0}</A> ({3})</DIV><DIV CLASS=td><A CLASS='z-op' DIV=div_content_right URL='sdcp.cgi?call=device_info&id={4}'>{1}</A></DIV><DIV CLASS='td {5}'>{2}</DIV><DIV CLASS=td>".format(row['alias'],row['hostname'],row['end'],row['user_id'],row['device_id'],'' if row['valid'] == 1 else "orange'")
+  print "<DIV CLASS=tr><DIV CLASS=td><A CLASS='z-op' DIV=div_content_right URL='sdcp.cgi?call=users_info&id={3}'>{0}</A> ({3})</DIV><DIV CLASS=td><A CLASS='z-op' DIV=div_content_right URL='sdcp.cgi?call=device_info&id={4}'>{1}</A></DIV><DIV CLASS='td {5}'>{2}</DIV><DIV CLASS='td controls'>".format(row['alias'],row['hostname'],row['end'],row['user_id'],row['device_id'],'' if row['valid'] == 1 else "orange'")
   if int(cookie['id']) == row['user_id'] or row['valid'] == 0:
    print aWeb.button('add',    DIV='div_content_left', TITLE='Extend booking', URL='sdcp.cgi?call=bookings_list&op=extend&device_id=%i&user_id=%i'%(row['device_id'],row['user_id']))
    print aWeb.button('delete', DIV='div_content_left', TITLE='Remove booking', URL='sdcp.cgi?call=bookings_list&op=debook&device_id=%i&user_id=%i'%(row['device_id'],row['user_id']))
