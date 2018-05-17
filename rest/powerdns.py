8d7baf7b73c88e48b98b42e7504e297d97da5e1f
Zacharias El Banna <zacharias@elbanna.se>
2018-05-17 11:23:19 +0200

    fixes

--- rest/powerdns.py
+++ rest/powerdns.py
@@ -15,64 +15,6 @@ __add_globals__ = lambda x: globals().update(x)
 
 from sdcp.core.common import DB,SC
 
-############################### Tools #################################
-#
-#
-def dedup(aDict):
- """Function docstring for dedup. Removes name duplicates.. (assume order by name => duplicate names :-))
-
- Args:
-
- Output:
- """
- with DB(SC['powerdns']['database'],'localhost',SC['powerdns']['username'],SC['powerdns']['password']) as db:
-  db.do("SELECT id,name,content FROM records WHERE type = 'A' OR type = 'PTR' ORDER BY name")   
-  rows = db.get_rows();
-  remove = []
-  previous = {'content':None,'name':None}
-  for row in rows:
-   if previous['content'] == row['content'] and previous['name'] == row['name']:
-    db.do("DELETE FROM records WHERE id = '{}'".format(row['id'] if row['id'] > previous['id'] else previous['id']))
-    row.pop('id',None)
-    remove.append(row)
-   else:
-    previous = row
- return {'removed':remove}
-
-#
-#
-def top(aDict):
- """Function docstring for top TBD
-
- Args:
-  - count (optional)
-
- Output:
- """
- def GL_get_host_name(aIP):
-  from socket import gethostbyaddr
-  try:    return gethostbyaddr(aIP)[0].partition('.')[0]
-  except: return None
-    
- count = int(aDict.get('count',10))
- fqdn_top = {}
- fqdn_who = {}
- with open(SC['powerdns']['logfile'],'r') as logfile:
-  for line in logfile:
-   parts = line.split()
-   if not parts[5] == 'Remote':
-    continue
-   fqdn  = parts[8].split('|')[0][1:]
-   fqdn_top[fqdn] = fqdn_top.get(fqdn,0)+1
-   fqdn_who[fqdn+"#"+parts[6]] = fqdn_who.get(fqdn+"#"+parts[6],0)+1
- from collections import Counter
- top = map(lambda x: {'fqdn':x[0],'count':x[1]}, Counter(fqdn_top).most_common(count))
- who = []
- for item in  Counter(fqdn_who).most_common(count):
-  parts = item[0].split('#')
-  who.append({'fqdn':parts[0], 'who':parts[1], 'hostname': GL_get_host_name(parts[1]), 'count':item[1]})
- return {'top':top,'who':who }
-
 #################################### Domains #######################################
 #
 #
@@ -225,3 +167,62 @@ def record_delete(aDict):
  with DB(SC['powerdns']['database'],'localhost',SC['powerdns']['username'],SC['powerdns']['password']) as db:
   deleted = db.do("DELETE FROM records WHERE id = '%s'"%(aDict['id']))
  return {'deleted':deleted}
+
+############################### Tools #################################
+#
+#
+def dedup(aDict):
+ """Function docstring for dedup. Removes name duplicates.. (assume order by name => duplicate names :-))
+
+ Args:
+
+ Output:
+ """
+ with DB(SC['powerdns']['database'],'localhost',SC['powerdns']['username'],SC['powerdns']['password']) as db:
+  db.do("SELECT id,name,content FROM records WHERE type = 'A' OR type = 'PTR' ORDER BY name")   
+  rows = db.get_rows();
+  remove = []
+  previous = {'content':None,'name':None}
+  for row in rows:
+   if previous['content'] == row['content'] and previous['name'] == row['name']:
+    db.do("DELETE FROM records WHERE id = '{}'".format(row['id'] if row['id'] > previous['id'] else previous['id']))
+    row.pop('id',None)
+    remove.append(row)
+   else:
+    previous = row
+ return {'removed':remove}
+
+#
+#
+def top(aDict):
+ """Function docstring for top TBD
+
+ Args:
+  - count (optional)
+
+ Output:
+ """
+ def GL_get_host_name(aIP):
+  from socket import gethostbyaddr
+  try:    return gethostbyaddr(aIP)[0].partition('.')[0]
+  except: return None
+    
+ count = int(aDict.get('count',10))
+ fqdn_top = {}
+ fqdn_who = {}
+ with open(SC['powerdns']['logfile'],'r') as logfile:
+  for line in logfile:
+   parts = line.split()
+   if not parts[5] == 'Remote':
+    continue
+   fqdn  = parts[8].split('|')[0][1:]
+   fqdn_top[fqdn] = fqdn_top.get(fqdn,0)+1
+   fqdn_who[fqdn+"#"+parts[6]] = fqdn_who.get(fqdn+"#"+parts[6],0)+1
+ from collections import Counter
+ top = map(lambda x: {'fqdn':x[0],'count':x[1]}, Counter(fqdn_top).most_common(count))
+ who = []
+ for item in  Counter(fqdn_who).most_common(count):
+  parts = item[0].split('#')
+  who.append({'fqdn':parts[0], 'who':parts[1], 'hostname': GL_get_host_name(parts[1]), 'count':item[1]})
+ return {'top':top,'who':who }
+
