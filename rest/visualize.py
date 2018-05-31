0979040145d0d93933bd70594a077266fe5409ea
Zacharias El Banna <zacharias@elbanna.se>
2018-05-31 14:00:01 +0200

    Visualize module

--- rest/visualize.py
+++ rest/visualize.py
@@ -40,182 +40,139 @@ def delete(aDict):
 
 #
 #
-def save(aDict):
- """ Saves config for later visualizer retrieval and edit
+def show(aDict):
+ """ Get map data from name or id
 
  Args:
-  - id (required)
-  - type (required)
-  - name (required)
-  - options (required)
-  - nodes (required)
-  - edges (required)
+  - id (optional required)
+  - name (optional required)
 
  Output:
-  - result
+  - name
+  - options
+  - edges
+  - nodes
  """
- name    = aDict['name']
- options = dumps(aDict['options'])
- nodes   = dumps(aDict['nodes'])
- edges   = dumps(aDict['edges'])
  ret = {}
  with DB() as db:
-  if aDict['type'] == 'network':
-   ret['update'] = db.do("UPDATE visualize SET name='%s', options='%s', nodes='%s', edges='%s' WHERE id = %s"%(name,options,nodes,edges,aDict['id']))
-   ret['id'] = aDict['id']
-   ret['result'] = 'OK' if ret['update'] > 0 else 'NOT_OK'
-  else:
-   ret['insert'] = db.do("INSERT INTO visualize (name,options,nodes,edges) VALUES('%s','%s','%s','%s') ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id), name=name"%(name,options,nodes,edges))
-   ret['id'] = db.get_last_id()
-   ret['result'] = 'OK' if ret['insert'] == 1 else 'EXISTED'
+  search = "id = %s"%aDict['id'] if aDict.get('id') else "name = '%s'"%aDict['name']
+  ret['xist'] = db.do("SELECT * FROM visualize WHERE %s"%search)
+  if ret['xist'] > 0:
+   data = db.get_row() 
+   ret['id']   = data['id']
+   ret['name'] = data['name']
+   for var in ['nodes','edges','options']:
+    ret[var] = loads(data[var])
  return ret
 
 #
 #
 def info(aDict):
- """ Info function to update information for specific id
+ """ Function to view or update information for specific map id or generate map for device id/ip
+
  Args:
-  - id (required)
-  - name (required)
-  - options (required)
-  - nodes (required)
-  - edges (required)
+  - id (optional required)
+  - op (optional)
+  - type (required) 'map/device'
+  - ip (optional required). IP of device, valid only for type 'device'
+  - diameter (optional) integer from 1-3, defaults to 2 to build graph for type 'device'
+  - name (optional)
+  - options (optional)
+  - nodes (optional)
+  - edges (optional)
 
  Output:
-  - result
- """
- id = aDict.pop('id',0)
- op = aDict.pop('op',None)
- ret = {'result':None}
- with DB() as db:
-  if op == 'update':
-   if id == 'new':
-    ret['insert']= db.do("INSERT INTO visualize (name,options,nodes,edges) VALUES('%s','%s','%s','%s')"%(aDict['name'],aDict['options'],aDict['nodes'],aDict['edges']))
-    id = db.get_last_id()
-    ret['result']= 'insert (%s)'%(ret['insert'])
-   else:
-    ret['update']= db.do("UPDATE visualize SET options='%s',nodes='%s',edges='%s' WHERE id = %s"%(aDict['options'],aDict['nodes'],aDict['edges'],id))
-    ret['result']= 'update (%s)'%(ret['update'] > 0)
-  ret['xist'] = db.do("SELECT * FROM visualize WHERE id = %s"%id)
-  data = db.get_row() if ret['xist'] > 0 else {}
-  ret['name'] = data.get('name',"")
-  for var in ['nodes','edges','options']:
-   ret[var] = loads(data.get(var,'null'))
-  ret['id'] = id
- return ret
- 
-
-#
-#
-def network(aDict):
- """ Function produces a dictionary tree of edges and nodes based on saved config
-
- Args:
   - id
- 
- Output:
+  - result
+  - type
   - name
   - options
   - nodes
   - edges
  """
- ret = {}
+ args = dict(aDict)
+ op   = args.pop('op',None)
+ ret = {'id':args.pop('id',0),'result':None,'type':args.pop('type','map')}
  with DB() as db:
-  ret['xist'] = db.do("SELECT * FROM visualize WHERE id = %s"%aDict['id'])
-  data = db.get_row()
-  ret['name'] = data.get('name',"")
-  for var in ['nodes','edges','options']:
-   ret[var] = loads(data.get(var,""))
-  ret['options']['physics'] = {'enabled':True, 'stabilization':{'onlyDynamicEdges':True}}
-  ret['options']['nodes']['image'] = 'images/viz-generic.png'
-  ret['result']  = 'OK' if ret['xist'] > 0 else 'NON_EXISTANT'
- return ret
+  if op == 'update':
+   if ret['id'] == 'new' or ret['type'] == 'device':
+    ret['insert']= db.do("INSERT INTO visualize (name,options,nodes,edges) VALUES('%s','%s','%s','%s')"%(args['name'],args['options'],args['nodes'],args['edges']))
+    ret['id']    = db.get_last_id()
+    ret['type']  = 'map'
+    ret['result']= 'insert (%s)'%(ret['insert'])
+   else:
+    ret['update']= db.do("UPDATE visualize SET name='%s',options='%s',nodes='%s',edges='%s' WHERE id = %s"%(args['name'],args['options'],args['nodes'],args['edges'],ret['id']))
+    ret['result']= 'update (%s)'%(ret['update'] > 0)
 
-#
-#
-def device(aDict):
- """ Function produces a dictionary tree of edges and nodes based on device id or ip
-
- Args:
-  - id (optional required)
-  - ip (optional required)
-  - diameter (optional) integer from 1-3, defaults to 2 to build graph
-
- Output:
-  - name
-  - options
-  - edges
-  - nodes  
- """
- ret = {}
- with DB() as db:
-  if aDict.get('ip'):
-   db.do("SELECT id FROM devices WHERE ip = INET_ATON('%s')"%aDict['ip'])
-   id = db.get_val('id')
+  ret['id'] = int(ret['id'])
+  if ret['type'] == 'map':
+   ret['xist'] = db.do("SELECT * FROM visualize WHERE id = %s"%ret['id'])
+   data = db.get_row() if ret['xist'] > 0 else {}
+   ret['name'] = data.get('name',"")
+   for var in ['nodes','edges','options']:
+    ret[var] = loads(data.get(var,'null'))
   else:
-   id = int(aDict['id'])
+   if args.get('ip'):
+    db.do("SELECT id FROM devices WHERE ip = INET_ATON('%s')"%args['ip'])
+    ret['id'] = db.get_val('id')
  
-  devices = {id:{'processed':False,'interfaces':0,'multipoint':0,'distance':0,'uplink':0}}
-  interfaces = []
-  ret['options'] = {'edges': {'length': 120, 'smooth':{'type': 'dynamic'}}, 'nodes': {'shadow': True, 'font':'14px verdana blue','shape':'image' }}
+   devices = {ret['id']:{'processed':False,'interfaces':0,'multipoint':0,'distance':0,'uplink':0}}
+   interfaces = []
+   ret['options'] = {'physics':{'enabled':True,'stabilization':{'onlyDynamicEdges':True}},'edges': {'length': 120, 'smooth':{'type': 'dynamic'}}, 'nodes': {'shadow': True, 'font':'14px verdana blue','shape':'image','image':'images/viz-generic.png' }}
 
-  # Connected and Multipoint
-  sql_connected  = "SELECT CAST({0} AS UNSIGNED) AS a_device, dc.id AS a_interface, dc.name AS a_name, dc.snmp_index AS a_index, dc.peer_interface AS b_interface, peer.snmp_index AS b_index, peer.name AS b_name, peer.device_id AS b_device FROM device_interfaces AS dc LEFT JOIN device_interfaces AS peer ON dc.peer_interface = peer.id WHERE dc.peer_interface IS NOT NULL AND dc.device_id = {0}"
-  sql_multipoint = "SELECT CAST({0} AS UNSIGNED) AS a_device, dc.id AS a_interface, dc.name AS a_name, dc.snmp_index AS a_index FROM device_interfaces AS dc WHERE dc.multipoint = 1 AND dc.device_id = {0}"
-  sql_multi_peer = "SELECT dc.id AS b_interface, dc.snmp_index AS b_index, dc.device_id AS b_device, dc.name AS b_name FROM device_interfaces AS dc WHERE dc.peer_interface = {0}"
-  for lvl in range(0,aDict.get('diameter',2)):
-   new_dev = {}
-   new_int = []
-   for key,dev in devices.iteritems():
-    if not dev['processed']:
-     # Find straight interfaces
-     dev['interfaces'] += db.do(sql_connected.format(key))
-     cons = db.get_rows()
+   # Connected and Multipoint
+   sql_connected  = "SELECT CAST({0} AS UNSIGNED) AS a_device, dc.id AS a_interface, dc.name AS a_name, dc.snmp_index AS a_index, dc.peer_interface AS b_interface, peer.snmp_index AS b_index, peer.name AS b_name, peer.device_id AS b_device FROM device_interfaces AS dc LEFT JOIN device_interfaces AS peer ON dc.peer_interface = peer.id WHERE dc.peer_interface IS NOT NULL AND dc.device_id = {0}"
+   sql_multipoint = "SELECT CAST({0} AS UNSIGNED) AS a_device, dc.id AS a_interface, dc.name AS a_name, dc.snmp_index AS a_index FROM device_interfaces AS dc WHERE dc.multipoint = 1 AND dc.device_id = {0}"
+   sql_multi_peer = "SELECT dc.id AS b_interface, dc.snmp_index AS b_index, dc.device_id AS b_device, dc.name AS b_name FROM device_interfaces AS dc WHERE dc.peer_interface = {0}"
+   for lvl in range(0,args.get('diameter',2)):
+    new_dev = {}
+    new_int = []
+    for key,dev in devices.iteritems():
+     if not dev['processed']:
+      # Find straight interfaces
+      dev['interfaces'] += db.do(sql_connected.format(key))
+      cons = db.get_rows()
 
-     # Find local multipoint
-     dev['multipoint'] += db.do(sql_multipoint.format(key))
-     multipoints = db.get_rows()
-     # Check 'other side' and add that interface
-     for interface in multipoints:
-      db.do(sql_multi_peer.format(interface['a_interface']))
-      peers = db.get_rows()
-      for peer in peers:
-       peer.update(interface)
-      cons.extend(peers)
+      # Find local multipoint
+      dev['multipoint'] += db.do(sql_multipoint.format(key))
+      multipoints = db.get_rows()
+      # Check 'other side' and add that interface
+      for interface in multipoints:
+       db.do(sql_multi_peer.format(interface['a_interface']))
+       peers = db.get_rows()
+       for peer in peers:
+        peer.update(interface)
+       cons.extend(peers)
 
-     # Deduce if interface is registered (the hard way, as no double dictionary)
-     for con in cons:
-      seen = devices.get(con['b_device'])
-      # If not seen before:
-      #  - Add device to process next round and save interface
-      #  -- in case already found as new, it means there is already such a connection, find that one and make both interfaces 'dynamic'
-      #  - Else, check if processed (then those interfaces are already added) if not add interface
-      # 1) For straight interface we cover 'back' since the dev is processed
-      # 2) For multipoint the other side will (ON SQL) see a straight interface so also covered by processed
-      # 3) There is no multipoint to multipoint
-      if not seen:
-       new = new_dev.get(con['b_device'],{'processed':False,'interfaces':0,'multipoint':0,'distance':lvl + 1,'uplink':0})
-       new['uplink'] += 1
-       new_dev[con['b_device']] = new
-       new_int.append(con)
-      elif not seen['processed']:
-       # exist but interface has not been registered, this covers loops as we set processed last :-)
-       new_int.append(con)
+      # Deduce if interface is registered (the hard way, as no double dictionary)
+      for con in cons:
+       seen = devices.get(con['b_device'])
+       # If not seen before:
+       #  - Add device to process next round and save interface
+       #  -- in case already found as new, it means there is already such a connection, find that one and make both interfaces 'dynamic'
+       #  - Else, check if processed (then those interfaces are already added) if not add interface
+       # 1) For straight interface we cover 'back' since the dev is processed
+       # 2) For multipoint the other side will (ON SQL) see a straight interface so also covered by processed
+       # 3) There is no multipoint to multipoint
+       if not seen:
+        new = new_dev.get(con['b_device'],{'processed':False,'interfaces':0,'multipoint':0,'distance':lvl + 1,'uplink':0})
+        new['uplink'] += 1
+        new_dev[con['b_device']] = new
+        new_int.append(con)
+       elif not seen['processed']:
+        # exist but interface has not been registered, this covers loops as we set processed last :-)
+        new_int.append(con)
 
-     dev['processed'] = True
-   # After all device leafs are processed we can add (net) new devices
-   devices.update(new_dev)
-   interfaces.extend(new_int)
+      dev['processed'] = True
+    # After all device leafs are processed we can add (net) new devices
+    devices.update(new_dev)
+    interfaces.extend(new_int)
 
-  # Now update devices with hostname and type and model, rename fields to fitting info
-  db.do("SELECT devices.id, hostname AS label, model, icon AS image FROM devices LEFT JOIN device_types ON devices.type_id = device_types.id WHERE devices.id IN (%s)"%(",".join(str(x) for x in devices.keys())))
-  nodes = db.get_rows()
-  for node in nodes:
-   devices[node['id']]['hostname'] = node['label']
-
- # Finalize
- ret['nodes'] = nodes
- ret['name']  = devices[id]['hostname']
- ret['edges'] = [{'from':intf['a_device'],'to':intf['b_device'],'title':"%s:%s <-> %s:%s"%(devices[intf['a_device']]['hostname'],intf['a_name'],devices[intf['b_device']]['hostname'],intf['b_name'])} for intf in interfaces]
- print dumps(devices,indent=4)
+   # Now update devices with hostname and type and model, rename fields to fitting info
+   db.do("SELECT devices.id, hostname AS label, model, icon AS image FROM devices LEFT JOIN device_types ON devices.type_id = device_types.id WHERE devices.id IN (%s)"%(",".join(str(x) for x in devices.keys())))
+   ret['nodes'] = db.get_rows()
+   for node in ret['nodes']:
+    devices[node['id']]['hostname'] = node['label']
+   ret['name']  = devices[ret['id']]['hostname']
+   ret['edges'] = [{'from':intf['a_device'],'to':intf['b_device'],'title':"%s:%s <-> %s:%s"%(devices[intf['a_device']]['hostname'],intf['a_name'],devices[intf['b_device']]['hostname'],intf['b_name'])} for intf in interfaces]
  return ret
--- site/visualize.py
+++ site/visualize.py
@@ -18,32 +18,12 @@ def list(aWeb):
  print "<DIV CLASS=thead><DIV CLASS=th>Id</DIV><DIV CLASS='th maxed'>Name</DIV><DIV CLASS=th STYLE='width:50px'>&nbsp;</DIV></DIV>"
  print "<DIV CLASS=tbody>"
  for map in res['maps']:
-  print "<DIV CLASS=tr><DIV CLASS=td>%s</DIV><DIV CLASS=td><A CLASS='z-op' DIV='div_content_right' URL='sdcp.cgi?visualize_network&type=network&id=%s'>%s</A></DIV><DIV CLASS=td><DIV CLASS='controls'>"%(map['id'],map['id'],map['name'])
-  print aWeb.button('document', DIV='div_content_right', URL='sdcp.cgi?visualize_info&id=%s'%map['id'],   TITLE='Show config') 
+  print "<DIV CLASS=tr><DIV CLASS=td>%s</DIV><DIV CLASS=td><A CLASS='z-op' DIV='div_content_right' URL='sdcp.cgi?visualize_show&id=%s'>%s</A></DIV><DIV CLASS=td><DIV CLASS='controls'>"%(map['id'],map['id'],map['name'])
+  print aWeb.button('edit',  DIV='div_content_right', URL='sdcp.cgi?visualize_info&type=map&id=%s'%map['id'],   TITLE='Show and Edit map') 
   print "</DIV></DIV></DIV>"
  print "</DIV></DIV></ARTICLE></SECTION>"
  print "<SECTION CLASS=content-right ID=div_content_right></SECTION>"
 
-
-#
-#
-def info(aWeb):
- from json import dumps
- args = aWeb.get_args2dict()
- res = aWeb.rest_call("visualize_info",args)
- print "<ARTICLE><P>Info for %s</P><DIV CLASS=controls>"%(res['name'])
- print aWeb.button('reload', DIV='div_content_right', URL='sdcp.cgi?visualize_info&id=%s'%aWeb['id'])
- print aWeb.button('trash',  DIV='div_content_right', URL='sdcp.cgi?visualize_delete&id=%s'%aWeb['id'], TITLE='Delete map') 
- print aWeb.button('save',   DIV='div_content_right', URL='sdcp.cgi?visualize_info&op=update', FRM='network_config')
- print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.config' DIV='div_options'>Options</A>"
- print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.config' DIV='div_nodes'>Nodes</A>"
- print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.config' DIV='div_edges'>Edges</A>"
- print "</DIV><SPAN CLASS=results>%s</SPAN><FORM ID='network_config'><INPUT TYPE=HIDDEN NAME=id VALUE=%s>"%(res.get('result'),aWeb['id'])
- print "<DIV CLASS='config' ID='div_options'><TEXTAREA CLASS=maxed STYLE='height:400px' NAME=options>%s</TEXTAREA></DIV>"%dumps(res['options'],indent=4)
- print "<DIV CLASS='config' STYLE='display:none' ID='div_nodes'><TEXTAREA CLASS=maxed STYLE='height:400px' NAME=nodes>%s</TEXTAREA></DIV>"%dumps(res['nodes'],indent=4)
- print "<DIV CLASS='config' STYLE='display:none' ID='div_edges'><TEXTAREA CLASS=maxed STYLE='height:400px' NAME=edges>%s</TEXTAREA></DIV>"%dumps(res['edges'],indent=4)
- print "</FORM>"
- print "</ARTICLE>"
 #
 #
 def delete(aWeb):
@@ -52,28 +32,50 @@ def delete(aWeb):
 
 #
 #
-def network(aWeb):
+def show(aWeb):
  from json import dumps
- id = aWeb['id']
- res = aWeb.rest_call("visualize_%s"%aWeb['type'],{'id':id})
- print "<ARTICLE><P>'%s' network</P><DIV CLASS=controls>"%(res['name'])
- print aWeb.button('reload', DIV='div_content_right', URL='sdcp.cgi?visualize_network&type=%s&id=%s'%(aWeb['type'],id), TITLE='Reload')
- if aWeb['type'] == 'device':
-  print aWeb.button('back', DIV='div_content_right', URL='sdcp.cgi?device_info&id=%s'%id, TITLE='Back')
+ res = aWeb.rest_call("visualize_show",{'id':aWeb['id']})
+ print "<ARTICLE><DIV CLASS='network' ID='div_network'></DIV><SCRIPT>"
+ print "var nodes = new vis.DataSet(%s);"%dumps(res['nodes'])
+ print "var edges = new vis.DataSet(%s);"%dumps(res['edges'])
+ print "var options = %s;"%(dumps(res['options']))
+ print "var data = {nodes:nodes, edges:edges};"
+ print "var network = new vis.Network(document.getElementById('div_network'), data, options);"
+ print "network.on('stabilizationIterationsDone', function () { network.setOptions({ physics: false }); });"
+ print "</SCRIPT></ARTICLE>"
+
+#
+#
+def info(aWeb):
+ from json import dumps
+ args = aWeb.get_args2dict()
+ res = aWeb.rest_call("visualize_info",args)
+ print "<ARTICLE><P>Info for %s</P><DIV CLASS=controls>"%(res['name'])
+ print aWeb.button('reload', DIV='div_content_right', URL='sdcp.cgi?visualize_info&type=%s&id=%s'%(res['type'],res['id']))
+ print aWeb.button('trash',  DIV='div_content_right', URL='sdcp.cgi?visualize_delete&id=%s'%res['id'], TITLE='Delete map') 
  print aWeb.button('document', onclick='network_edit()', TITLE='Enable editor')
- print aWeb.button('start',  onclick='network_start()')
- print aWeb.button('stop',   onclick='network_stop()')
- print aWeb.button('save',   onclick='network_save()')
- print "</DIV><LABEL FOR=network_name>Name:</LABEL><INPUT TYPE=TEXT CLASS=background STYLE='width:120px' VALUE='%s' ID=network_name><SPAN CLASS='results' ID=network_result></SPAN>"%res['name']
- print "<INPUT TYPE=HIDDEN VALUE=%s ID=network_id>"%id
- print "<INPUT TYPE=HIDDEN VALUE=%s ID=network_type>"%aWeb['type']
- print "<DIV ID='device_network' CLASS='network'></DIV><SCRIPT>"
+ print aWeb.button('start',  onclick='network_start()', TITLE='Enable physics')
+ print aWeb.button('stop',   onclick='network_stop()', TITLE='Disable physics')
+ print aWeb.button('sync',   onclick='network_sync()',TITLE='Sync graph to config')
+ print aWeb.button('save',   DIV='div_content_right', URL='sdcp.cgi?visualize_info&op=update', FRM='network_config', TITLE='Save config')
+ print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.tab' DIV='div_network'><IMG SRC='images/btn-network.png'></A>"
+ print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.tab' DIV='div_options'>Options</A>"
+ print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.tab' DIV='div_nodes'>Nodes</A>"
+ print "<A CLASS='z-op btn small text' OP='single' SELECTOR='.tab' DIV='div_edges'>Edges</A>"
+ print "</DIV><LABEL FOR=name>Name:</LABEL><FORM ID='network_config'><INPUT TYPE=TEXT CLASS=background STYLE='width:120px' VALUE='%s' NAME=name><SPAN ID=result CLASS=results>%s</SPAN>"%(res['name'],res.get('result'))
+ print "<DIV CLASS='tab' STYLE='display:none' ID='div_options'><TEXTAREA CLASS=maxed STYLE='height:400px' ID=options NAME=options>%s</TEXTAREA></DIV>"%dumps(res['options'],indent=4)
+ print "<DIV CLASS='tab' STYLE='display:none' ID='div_nodes'><TEXTAREA CLASS=maxed STYLE='height:400px' ID=nodes NAME=nodes>%s</TEXTAREA></DIV>"%dumps(res['nodes'],indent=4)
+ print "<DIV CLASS='tab' STYLE='display:none' ID='div_edges'><TEXTAREA CLASS=maxed STYLE='height:400px' ID=edges NAME=edges>%s</TEXTAREA></DIV>"%dumps(res['edges'],indent=4)
+ print "<INPUT TYPE=HIDDEN VALUE='%s' NAME=id>"%res['id']
+ print "<INPUT TYPE=HIDDEN VALUE='%s' NAME=type>"%res['type']
+ print "</FORM>"
+ print "<DIV CLASS='tab network' ID='div_network'></DIV><SCRIPT>"
  print "var nodes = new vis.DataSet(%s);"%dumps(res['nodes'])
  print "var edges = new vis.DataSet(%s);"%dumps(res['edges'])
  print "var options = %s;"%(dumps(res['options']))
  print """
  var data = {nodes:nodes, edges:edges};
- var network = new vis.Network(document.getElementById('device_network'), data, options);
+ var network = new vis.Network(document.getElementById('div_network'), data, options);
  network.on('stabilizationIterationsDone', function () { network.setOptions({ physics: false }); });
 
  function network_start(){ 
@@ -88,23 +90,25 @@ def network(aWeb):
   network.setOptions({ manipulation:{ enabled:true }});
  };
 
- function network_save(){
+ function network_sync(){
   network.storePositions();
-  var output = { options:options,edges:[],nodes:[],name:$('#network_name').val(),id:$('#network_id').val(), type:$('#network_type').val()};
-  Object.entries(nodes._data).forEach(([key,value]) => {
+  var sync = { edges:[],nodes:[]};
+  Object.entries(network.body.data.nodes._data).forEach(([key,value]) => {
    var node = value;
    node.x   = value.x;
    node.y   = value.y;
-   output.nodes.push(node);
+   sync.nodes.push(node);
   });
-  Object.entries(edges._data).forEach(([key,value]) => {
+  Object.entries(network.body.data.edges._data).forEach(([key,value]) => {
    var edge   = {};
    edge.from  = value.from;
    edge.to    = value.to;
    edge.title = value.title;
-   output.edges.push(edge);
+   sync.edges.push(edge);
   });
-  $.post('rest.cgi?visualize_save',JSON.stringify(output), result => { $('#network_result').html(JSON.stringify(result)); });
+  $("#options").val(JSON.stringify(options,undefined,4));
+  $("#nodes").val(JSON.stringify(sync.nodes,undefined,4));
+  $("#edges").val(JSON.stringify(sync.edges,undefined,4));
  };
  """
  print "</SCRIPT></ARTICLE>"
