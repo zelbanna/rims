c5b1603031c8c22df3e20f158c921161eff9314d
Juniper Stolabs <stolabs@juniper.net>
2018-06-20 22:59:58 +0200

    bug on multihomed

--- rest/visualize.py
+++ rest/visualize.py
@@ -115,7 +115,7 @@ def network(aDict):
    if args.get('ip'):
     db.do("SELECT devices.id FROM devices LEFT JOIN ipam_addresses AS ia ON ia.id = devices.ipam_id WHERE ia.ip = INET_ATON('%(ip)s')"%args)
     ret['id'] = db.get_val('id')
- 
+
    devices = {ret['id']:{'processed':False,'interfaces':0,'multipoint':0,'distance':0,'uplink':0}}
    interfaces = []
    ret['options'] = {'layout':{'randomSeed':2}, 'physics':{'enabled':True }, 'edges':{'length':180}, 'nodes': {'shadow': True, 'font':'14px verdana blue','shape':'image','image':'images/viz-generic.png' }}
@@ -125,18 +125,20 @@ def network(aDict):
    sql_multipoint = "SELECT CAST({0} AS UNSIGNED) AS a_device, dc.id AS a_interface, dc.name AS a_name, dc.snmp_index AS a_index FROM device_interfaces AS dc WHERE dc.multipoint = 1 AND dc.device = {0}"
    sql_multi_peer = "SELECT dc.id AS b_interface, dc.snmp_index AS b_index, dc.device AS b_device, dc.name AS b_name FROM device_interfaces AS dc WHERE dc.peer_interface = {0}"
    for lvl in range(0,args.get('diameter',2)):
-    new_dev = {}
-    new_int = []
-    for from_id,from_dev in devices.iteritems():
+    level_ids = devices.keys()
+    # We cannot iterate as the current data format required that we add each new device found, for each id - otherwise multihomed get's screwed
+    for from_id in level_ids:
+     from_dev = devices[from_id]
      if not from_dev['processed']:
+      new_dev = {}
+      new_int = []
       # Find straight interfaces
       from_dev['interfaces'] += db.do(sql_connected.format(from_id))
       cons = db.get_rows()
-
       # Find local multipoint
       from_dev['multipoint'] += db.do(sql_multipoint.format(from_id))
       multipoints = db.get_rows()
-      # Check 'other side' and add that interface
+      # Check 'other side' and find peer interfaces, add local interface info to new 'peer' interfaces and insert into local's list
       for interface in multipoints:
        db.do(sql_multi_peer.format(interface['a_interface']))
        peers = db.get_rows()
@@ -150,10 +152,10 @@ def network(aDict):
        seen = devices.get(to)
        # If not seen before:
        #  - Add device to process next round and save interface
-       #  -- in case already found as new, it means there is already such a connection, find that one and make both interfaces 'dynamic'
+       #  -- in case already found as new, it means there is already such a connection, find that one and make interfaces 'multihomed' ('dynamic')
        #  - Else, check if processed (then those interfaces are already added) if not add interface
        # 1) For straight interface we cover 'back' since the dev is processed
-       # 2) For multipoint the other side will (ON SQL) see a straight interface so also covered by processed
+       # 2) For multipoint the other side will (through SQL) see a straight interface so also covered by processed
        # 3) There is no multipoint to multipoint
        if not seen:
         new = new_dev.get(to,{'processed':False,'interfaces':0,'multipoint':0,'distance':lvl + 1,'uplink':0})
@@ -163,15 +165,16 @@ def network(aDict):
        elif not seen['processed']:
         # exist but interface has not been registered, this covers loops as we set processed last :-)
         new_int.append(con)
-
+       else:
+        pass
       from_dev['processed'] = True
-    # After all device leafs are processed we can add (net) new devices
-    devices.update(new_dev)
-    # Check if a connection is multihomed, then make dynamic, else straight
-    multihomed = [k for k,v in new_dev.iteritems() if v['uplink'] > 1]
-    for con in new_int:
-     con['type'] = {'type':'dynamic'} if con['b_device'] in multihomed else False
-    interfaces.extend(new_int)
+      # Now add new devices
+      devices.update(new_dev)
+      # Check if a connection is multihomed, then make dynamic, else straight
+      multihomed = [k for k,v in new_dev.iteritems() if v['uplink'] > 1]
+      for con in new_int:
+       con['type'] = {'type':'dynamic'} if con['b_device'] in multihomed else False
+      interfaces.extend(new_int)
 
    # Now update devices with hostname and type and model, rename fields to fitting info
    db.do("SELECT devices.id, hostname AS label, model, icon AS image FROM devices LEFT JOIN device_types ON devices.type_id = device_types.id WHERE devices.id IN (%s)"%(",".join(str(x) for x in devices.keys())))
