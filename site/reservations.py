df092c457fda041c23d49a58c5a40ec157143c64
Zacharias El Banna <zacharias@elbanna.se>
2018-08-20 23:24:00 +0200

    bug

--- site/reservations.py
+++ site/reservations.py
@@ -30,7 +30,7 @@ def list(aWeb):
   print "<DIV CLASS=tr><DIV CLASS=td><A CLASS='z-op' DIV=div_content_right URL='zdcp.cgi?users_info&id={3}'>{0}</A> ({3})</DIV><DIV CLASS=td><A CLASS='z-op' DIV=div_content_right URL='zdcp.cgi?device_info&id={4}'>{1}</A></DIV><DIV CLASS='td {5}'>{2}</DIV><DIV CLASS='td controls'>".format(row['alias'],row['hostname'],row['end'],row['user_id'],row['device_id'],'' if row['valid'] == 1 else "orange'")
   if int(cookie['id']) == row['user_id'] or row['valid'] == 0:
    print aWeb.button('add',    DIV='div_content', TITLE='Extend reservation', URL='zdcp.cgi?reservations_list&op=extend&device_id=%i&user_id=%i'%(row['device_id'],row['user_id']))
-   print aWeb.button('delete', DIV='div_content', TITLE='Remove reservation', URL='zdcp.cgi?reservations_list&op=debook&device_id=%i&user_id=%i'%(row['device_id'],row['user_id']))
+   print aWeb.button('delete', DIV='div_content', TITLE='Remove reservation', URL='zdcp.cgi?reservations_list&op=drop&device_id=%i&user_id=%i'%(row['device_id'],row['user_id']))
   print "&nbsp;</DIV></DIV>"
  print "</DIV></DIV></ARTICLE></SECTION>"
  print "<SECTION CLASS=content-right ID=div_content_right></SECTION>"
