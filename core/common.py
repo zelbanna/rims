99cfd482c802bc7cfd6cbb5287ebb072aa3875b9
Zacharias El Banna <zacharias@elbanna.se>
2018-08-20 23:22:44 +0200

    tweak

--- core/common.py
+++ core/common.py
@@ -184,5 +184,4 @@ def basic_auth(aUsername,aPassword):
 # HTML Code translator
 #
 def code_to_string(aCode):
- codes = {200:'OK',201:'Created',204:'No Content',304:'Not Modified',400:'Bad Request',401:'Unauthorized',403:'Forbidden',404:'Not Found',500:'Internal Server Error',591:'Z-Exception'}
- return codes.get(aCode,'%s Code Not Encoded'%aCode)
+ return {200:'OK',201:'Created',204:'No Content',304:'Not Modified',400:'Bad Request',401:'Unauthorized',403:'Forbidden',404:'Not Found',500:'Internal Server Error',591:'Z-Exception'}.get(aCode,'%s Code Not Encoded'%aCode)
