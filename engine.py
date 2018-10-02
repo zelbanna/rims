32086f595945ba18f9ffc1543c728d2a8395101d
Zacharias El Banna <zacharias@elbanna.se>
2018-10-02 22:25:22 +0200

    thread opt

--- engine.py
+++ engine.py
@@ -11,9 +11,10 @@ __status__ = "Production"
 
 
 from os import path as ospath
-from sys import path as syspath, exit
+from sys import path as syspath, exit, setcheckinterval
 basepath = ospath.abspath(ospath.join(ospath.dirname(__file__), '..'))
 syspath.insert(1, basepath)
+setcheckinterval(200)
 from zdcp.core.engine import start
 start(20)
 exit(1)
