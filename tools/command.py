bd5f6a54cc831592ac934a541b2869561d06615c
Juniper Stolabs <stolabs@juniper.net>
2018-10-15 11:21:01 +0200

    command based debug

---
+++ tools/command.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+__author__ = "Zacharias El Banna"
+__version__ = "5.2GA"
+__status__ = "Production"
+
+from os   import path as ospath
+from sys  import path as syspath, exit, argv
+syspath.append(ospath.abspath(ospath.join(ospath.dirname(__file__), '..','..')))
+
+if __name__ == "__main__":
+ if len(argv) < 3:
+  print(argv[0] + " <settings.json> func [<json-arg>]")
+  exit(0)
+
+ from json import loads, dumps, load
+ from time import time
+ from importlib import import_module
+ from zdcp.core.engine import Context
+ from zdcp.core.common import DB, rest_call
+ with open(argv[1]) as f:
+  file = load(f)
+ settings = {}
+ for section,data in file.items():
+  if not settings.get(section):
+   settings[section] = {}
+  for key,val in data.items():
+   settings[section][key] = val['value']
+ system = settings['system']
+ port = system['port']
+ if system['id'] == 'master':
+  with DB(system['db_name'], system['db_host'], system['db_user'], system['db_pass']) as db:
+   db.do("SELECT section,parameter,value FROM settings WHERE node = 'master'")
+   for row in db.get_rows():
+    if not settings.get(row['section']):
+     settings[row['section']] = {}
+    settings[row['section']][row['parameter']] = row['value']
+ else:
+  ext = rest_call("%s/settings/fetch/%s"%(system['master'],system['id']))['settings']
+  for section,data in ext.items():
+   if not settings.get(section):
+    settings[section] = {}
+   for key,val in data.items():
+    settings[section][key] = val
+
+ ctx = Context(settings,None)
+ timestamp = int(time())
+ try:    args = loads(argv[3])
+ except: args = {}
+ mod,_,fun = argv[2].partition('_')
+ started = "Executing:%s(%s)"%(fun,args)
+ print(started)
+ try:
+  module = import_module("zdcp.rest.%s"%mod)
+  output = getattr(module,fun,None)(args,ctx)
+ except Exception as e: output = e.args[0]
+ print("Time spent: %i\n%s\n%s"%(int(time()) - timestamp,'_'*len(started),dumps(output,indent=4, sort_keys=True)))
--- tools/rest.py
+++ tools/rest.py
@@ -5,34 +5,27 @@ __version__ = "5.2GA"
 __status__ = "Production"
 
 from os   import path as ospath
-from sys  import path as syspath
-from json import loads, dumps
-from time import time
+from sys  import path as syspath, argv, exit
 syspath.append(ospath.abspath(ospath.join(ospath.dirname(__file__), '..','..')))
 from zdcp.core.engine import Context
 
 if __name__ == "__main__":
- from json import dumps
- from sys import argv, exit
  if len(argv) < 2:
   print(argv[0] + " <URL> [<json-arg>]")
   exit(0)
+
+ from json import loads, dumps
+ from time import time
+ from zdcp.core.common import rest_call
+ timestamp = int(time())
+ try:    args = loads(argv[2])
+ except: args = {}
+ started = "Executing:%s(%s)"%(argv[1],args)
+ print(started)
+ try:  res = rest_call(argv[1],args, aTimeout = 300)
+ except Exception as e: output = e.args[0]
  else:
-  timestamp = int(time())
-  try: args = loads(argv[2])
-  except: args = {}
-  started = "Executing:%s(%s)"%(argv[1],args)
-  print(started)
-  from zdcp.core.common import rest_call
-  try:
-   res = rest_call(argv[1],args, aTimeout = 300) 
-  except Exception as e:
-   output = e.args[0]
-  else:
-   output = res['data']
-   for x in res['info'].items():
-    print("%s: %s"%x)
-  timespent = int(time()) - timestamp
-  print("Time spent: %i"%(timespent))
-  print("_" * len(started))
-  print(dumps(output,indent=4, sort_keys=True))
+  output = res['data']
+  for x in res['info'].items():
+   print("%s: %s"%x)
+ print("Time spent: %i\n%s\n%s"%(int(time()) - timestamp,'_'*len(started),dumps(output,indent=4, sort_keys=True)))
