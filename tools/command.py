23c3f0494f0b37fe5b6d350461068fa47215d834
Juniper Stolabs <stolabs@juniper.net>
2018-10-15 11:49:20 +0200

    WorkerDummy

--- tools/command.py
+++ tools/command.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
    for key,val in data.items():
     settings[section][key] = val
 
- ctx = Context(settings,None)
+ ctx = Context(settings,WorkerDummy(settings))
  timestamp = int(time())
  try:    args = loads(argv[3])
  except: args = {}
