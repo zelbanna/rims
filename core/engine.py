ae6cfa7181042d2b4648ad10369218b9fede9c46
Zacharias El Banna <zacharias@elbanna.se>
2018-11-01 23:08:26 +0100

    blue style sheet

--- core/engine.py
+++ core/engine.py
@@ -1,7 +1,7 @@
 """System engine"""
 __author__ = "Zacharias El Banna"
 __version__ = "5.5"
-__build__ = 119
+__build__ = 120
 
 __all__ = ['Context','WorkerPool']
 from json import loads, load, dumps
