6ac461e78dcbfac60c6d70eb893d212e4b7afa1a
Zacharias El Banna <zacharias@elbanna.se>
2018-11-02 16:24:29 +0100

    DB for themes

--- core/engine.py
+++ core/engine.py
@@ -1,7 +1,7 @@
 """System engine"""
 __author__ = "Zacharias El Banna"
 __version__ = "5.5"
-__build__ = 124
+__build__ = 125
 
 __all__ = ['Context','WorkerPool']
 from os import path as ospath, getpid, walk
