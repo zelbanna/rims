ff6557a2c0c39a7e829f9ad4f6e6ad235b776fab
Zacharias El Banna <zacharias@elbanna.se>
2018-11-02 10:19:52 +0100

    color fixes

--- images/color-convert.sh
+++ images/color-convert.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-# convert <color> <'#from'> <'#to'>
+# convert <color> <'#color code'>
+#
+# Using blue as basis for color conversion
 #
 # Using linux tool ImageMagick
 # https://www.imagemagick.org/Usage/color_basics
 #
-for file in btn*.$1.png;
+for file in btn*.blue.png;
 do
- echo "Converting $file"
- convert $file -fill $3 -opaque $2 $file.new.png
- rm $file
- mv $file.new.png $file
+ echo "Converting ${file%.blue.png}"
+ convert $file -fill $2 -opaque '#3E84F4' ${file%.blue.png}.$1.png
 done
-
