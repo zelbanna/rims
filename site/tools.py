74dfd687fb5cd55612068e67daf0b7e03da0ddf9
Zacharias El Banna <zacharias@elbanna.se>
2018-12-05 17:12:47 +0100

    fixes

--- site/tools.py
+++ site/tools.py
@@ -10,8 +10,8 @@ def main(aWeb):
  data = aWeb.rest_call("system/inventory",{'node':aWeb.node(),'user_id':cookie['id']})
  aWeb.wr("<NAV><UL>")
  if data.get('node'):
-  aWeb.wr("<LI><A CLASS=z-op DIV=div_content URL='servers_list'>Servers</A></LI>")
   aWeb.wr("<LI><A CLASS=z-op DIV=div_content URL='nodes_list'>Nodes</A></LI>")
+  aWeb.wr("<LI><A CLASS=z-op DIV=div_content URL='servers_list'>Servers</A></LI>")
  if data.get('users'):
   aWeb.wr("<LI><A CLASS=z-op DIV=div_content URL='users_list'>Users</A></LI>")
  tools = data.get('tools',[])
