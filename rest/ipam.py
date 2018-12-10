68b445df29808663be99b3685113437779b49f2b
Zacharias El Banna <zacharias@elbanna.se>
2018-12-10 14:47:17 +0100

    fixes

--- rest/ipam.py
+++ rest/ipam.py
@@ -456,6 +456,8 @@ def address_status_check(aCTX, aArgs = None):
   aDev['old'] = aDev['state']
   try:    aDev['state'] = 1 if (system("ping -c 1 -w 1 %s > /dev/null 2>&1"%(aDev['ip'])) == 0) else 2
   except: pass
+  if aDev['old'] != aDev['state']:
+   print("State change %s"%aDev)
   return True
 
  nworkers = max(20,int(aCTX.config['workers']) - 5)
@@ -489,6 +491,7 @@ def address_status_report(aCTX, aArgs = None):
   - up (number of updated up state)
   - down (number of updated down state)
  """
+ print("address_status_report => %s"%aArgs)
  ret = {}
  log = {}
  notify = aCTX.settings.get('notifier')
