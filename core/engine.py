47df4cc33ff5368b434b307edb035f6029f6d843
Zacharias El Banna <zacharias@elbanna.se>
2020-05-11 14:33:08 +0200

    fixes

--- core/engine.py
+++ core/engine.py
@@ -1,7 +1,7 @@
 """System engine"""
 __author__ = "Zacharias El Banna"
 __version__ = "6.3"
-__build__ = 315
+__build__ = 316
 __all__ = ['Context','WorkerPool']
 
 from os import path as ospath, getpid, walk, stat as osstat
