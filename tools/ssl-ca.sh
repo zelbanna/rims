736f82c396478d0de622b7bbf5e0dfdad8857911
Zacharias El Banna <zacharias@elbanna.se>
2020-06-02 13:53:54 +0200

    fixes

--- tools/ssl-ca.sh
+++ tools/ssl-ca.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-openssl genrsa -aes256 -out ../ca/CA-key.pem 4096
+#
+#
+#
+openssl genrsa -out ../ca/CA-key.pem 4096
 openssl rsa -in ../ca/CA-key.pem -outform PEM -pubout -out ../ca/CA.pub
-openssl req -new -key ../ca/CA-key.pem -x509 -days 1825 -out ../ca/CA-cert.pem
-openssl x509 -outform der -in ../ca/CA-cert.pem -out ../ca/CA-cert.crt
+openssl req -new -subj "C=SE/O=RIMS/CN=rims-ca" -key ../ca/CA-key.pem -x509 -days 1825 -out ../ca/CA-cert.pem
+openssl x509 -in ../ca/CA-cert.pem -outform der -out ../ca/CA-cert.crt
 cp ../ca/CA-cert.crt ../site/CA-cert.crt
 cp ../ca/CA-cert.crt ../static/CA-cert.crt
-
--- tools/ssl-server.sh
+++ tools/ssl-server.sh
@@ -2,15 +2,19 @@
 #
 # Generates both keys and cert
 #
+# $1 = CN
+# $2 = AltNames
+#
+echo "Creating CERT for $1 with Alternative Names $2"
+echo ""
 echo "********************* Creating Keys *********************"
-openssl genrsa -aes256 -out ../ssl/$1-key.pem 4096
+openssl genrsa -out ../ssl/$1-key.pem 4096
 
 echo "********************* CSR request ***********************"
 echo " -Do not assign password to cert! Press key to continue -"
 read
-openssl req -new -key ../ssl/$1-key.pem -out ../ssl/$1.csr
+openssl req -new -subj "/C=SE/O=RIMS/CN=$1" -key ../ssl/$1-key.pem -out ../ssl/$1.csr
 
 echo "********************* CA signing ************************"
-#openssl x509 -req -days 365 -in ../ssl/$1.csr -CA ../ca/CA-cert.pem -CAkey ../ca/CA-key.pem -CAcreateserial -out ../ssl/$1-cert.pem
 openssl x509 -req -days 365 -in ../ssl/$1.csr -CA ../ca/CA-cert.pem -CAkey ../ca/CA.key -CAcreateserial -out ../ssl/$1-cert.pem
 rm ../ssl/$1.csr
