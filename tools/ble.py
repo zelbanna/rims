a6c9b74b5015346843ca8f02da7d02e5495fe0c4
Zacharias El Banna <zacharias@elbanna.se>
2020-06-30 22:52:53 +0200

    fix

--- tools/ble.py
+++ tools/ble.py
@@ -17,7 +17,6 @@ parser.add_argument('-c','--config', help = 'Config unless config.json', default
 parser.add_argument('-d','--device', help = 'Device', required = False, default='hci0')
 parser.add_argument('-r','--report', help = 'Continuous reporting', required = False, action='store_true')
 parser.add_argument('-s','--sleep',  help = 'Sleeptime', required = False, default=40)
-parser.add_argument('-t','--timeout', help = 'Timeout for discovery', required = False, default=20)
 parser.add_argument('-u','--url',    help = 'RIMS URL', required = False, default='http://127.0.0.1:8080')
 parser.add_argument('-x','--deadtime', help = 'Timeout before declaring dead', required = False, default=300)
 args = parser.parse_args()
@@ -37,23 +36,8 @@ while True:
   print(f"BLE Error: {e}")
   sysexit(1)
  else:
-  up = []
-  dn = []
-  for k,v in data.items():
-   if discovered.pop(k,False):
-    v = 0
-   elif v < (int(args.deadtime) / (int(args.timeout) + int(args.sleep))):
-    v +=1
-   else:
-    dn.append(k)
-  for k in discovered.keys():
-   data[k] = 0
-   up.append(k)
-  for k in dn:
-   data.pop(k,None)
-  if dn or up or args.report:
-   try:
-    res = rest_call(f"{args.url}/internal/services/ble/report", aArgs = {'up':up,'down':dn}, aTimeout = 5, aHeader = {'X-Token':token, 'X-Log':False})
-   except Exception as e:
-    print(f"REST Error: {e}")
+  try:
+   res = rest_call(f"{args.url}/internal/services/ble/report", aArgs = {'devices':discovered}, aTimeout = 5, aHeader = {'X-Token':token, 'X-Log':False})
+  except Exception as e:
+   print(f"REST Error: {e}")
  sleep(int(args.sleep))
