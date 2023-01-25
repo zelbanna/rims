13e394d8ef5e55cafcbf59d7ee8317f643a245d6
zelbanna <zacharias@elbanna.se>
2023-01-25 22:59:59 +0100

    updated config and install

--- tools/debian.sh
+++ tools/debian.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Standalone server
 
-groupadd -g 900 docker
-useradd -g 900 -u 900 -M -s /usr/sbin/nologin docker
 mkdir /var/sockets
 
 echo "Starting installation - give root DB password on the next line and press enter"
@@ -31,19 +29,19 @@ docker network create --subnet=172.18.0.0/16 services
 echo "Creating containers"
 echo "MariaDB: 172.18.0.2"
 echo "InfluxDB: 172.18.0.3 -> add to config.json"
-echo "Grafana: 172.18.0.6 -> proxy to port 3000 locally"
 echo "PDNS Recursor: 172.18.0.4 -> configure API and add to config.json, proxy to port 53 locally"
 echo "PDNS Server: 172.18.0.5 -> configure API and add to config.json"
+echo "isc-dhcp-server: Connect to host"
 echo ""
 echo " - press enter to continue - "
 read
-docker create --name=mariadb -e PUID=900 -e PGID=900 -e MYSQL_ROOT_PASSWORD=$mariadbpassword -e TZ=Europe/Stockholm -v mariadb:/config --net services --ip 172.18.0.2 --restart unless-stopped linuxserver/mariadb
+docker create --name=mariadb -e MYSQL_ROOT_PASSWORD=$mariadbpassword -e TZ=Europe/Stockholm -v mariadb:/config --net services --ip 172.18.0.2 --restart unless-stopped linuxserver/mariadb
 docker start mariadb
-docker create --name=influxdb2 --user 900:900 -p 8086:8086/tcp -v influxdb2:/var/lib/influxdb2 -v influxdb2.conf.d:/etc/influxdb2 --net services --ip 172.18.0.3 --restart unless-stopped influxdb
-# docker create --name=grafana --user 900:900 -p 3000:3000/tcp -v grafana:/var/lib/grafana --net services --ip 172.18.0.6 --restart unless-stopped grafana/grafana
-docker create --name=pdns-server --user 900:root -v pdns-server:/etc/powerdns  -v /var/sockets/pdns-server:/var/run/pdns --net services --ip 172.18.0.5 --restart unless-stopped xddxdd/powerdns
+docker create --name=influxdb2 -p 8086:8086/tcp -v influxdb2:/var/lib/influxdb2 -v influxdb2.conf.d:/etc/influxdb2 --net services --ip 172.18.0.3 --restart unless-stopped influxdb
+docker create --name=pdns-server -v pdns-server:/etc/powerdns  -v /var/sockets/pdns-server:/var/run/pdns --net services --ip 172.18.0.5 --restart unless-stopped xddxdd/powerdns
 docker create --name=pdns-recursor -p 53:53 -p 53:53/udp -v pdns-recursor:/etc/powerdns  --net services --ip 172.18.0.4 --restart unless-stopped xddxdd/powerdns-recursor
-docker create --name=telegraf --user 900:900 -p 8092:8092/udp -p 8125:8125/udp -p 8094:8094/tcp -v telegraf:/etc/telegraf -v /var/run/docker.sock:/var/run/docker.sock -v /var/sockets:/var/sockets --net services --ip 172.18.0.7 --restart unless-stopped telegraf
+docker create --name=telegraf -p 8092:8092/udp -p 8125:8125/udp -p 8094:8094/tcp -v telegraf:/etc/telegraf -v /var/run/docker.sock:/var/run/docker.sock -v /var/sockets:/var/sockets --net services --ip 172.18.0.7 --restart unless-stopped telegraf
+docker create --name=dhcpd --net host -v dhcpd:/data --restart unless-stopped networkboot/dhcpd eth0
 wget https://raw.githubusercontent.com/PowerDNS/pdns/rel/rec-4.4.x/modules/gmysqlbackend/schema.mysql.sql
 
 mysql -h 172.18.0.2 -uroot -p$mariadbpassword << `END`
